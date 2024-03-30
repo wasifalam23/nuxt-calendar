d86984f7b5d7f62b60c3da4320d8ca67b05332ac
wasif_alam <wsfalam84@gmail.com>
2024-03-30 14:27:16 +0530

    add start & end date, fix other

--- components/DatePicker2.vue
+++ components/DatePicker2.vue
@@ -1,5 +1,5 @@
 <script setup>
-	import { onMounted } from "vue";
+	import moment from "moment";
 	import AirDatepicker from "air-datepicker";
 	import "air-datepicker/air-datepicker.css";
 	import localeEn from "air-datepicker/locale/en";
@@ -36,18 +36,18 @@
 
 	const selectedDate = ref(null);
 	const formattedDate = ref(null);
-	const selectedTimeIndex = ref(null);
-	const selectedTime = ref(null);
-	const timeSlot = ref([
-		"01.00 PM",
-		"02.00 PM",
-		"03.00 PM",
-		"04.00 PM",
-		"05.00 PM",
-		"06.00 PM",
-		"07.00 PM",
-		"08.00 PM",
-		"09.00 PM",
+	const selectedTimeSlotIndex = ref(null);
+	const selectedTimeSlot = ref(null);
+	const timeSlots = ref([
+		{ start: "10:00 AM", end: "11:00 AM" },
+		{ start: "01:00 PM", end: "02:00 PM" },
+		{ start: "02:00 PM", end: "03:00 PM" },
+		{ start: "03:00 PM", end: "04:00 PM" },
+		{ start: "04:00 PM", end: "05:00 PM" },
+		{ start: "05:00 PM", end: "06:00 PM" },
+		{ start: "06:00 PM", end: "07:00 PM" },
+		{ start: "07:00 PM", end: "08:00 PM" },
+		{ start: "08:00 PM", end: "09:00 PM" },
 	]);
 	const currentTab = ref(1);
 	const name = ref("");
@@ -86,8 +86,12 @@
 				);
 			});
 
-			const timeSlots = bookingsData.map((data) => data.time_slot);
-			disabledSlots.value = timeSlots;
+			const slots = bookingsData.map((slot) =>
+				toAMPMTime(slot.time_slot_start)
+			);
+
+			disabledSlots.value = slots;
+			console.log(disabledSlots.value);
 
 			isBookFetching.value = false;
 		} catch (err) {
@@ -122,16 +126,12 @@
 	}
 
 	function selectTimeSlot(index) {
-		selectedTimeIndex.value = index;
+		selectedTimeSlotIndex.value = index;
 	}
 
 	function nextClicked() {
-		if (selectedDate.value !== null && selectedTimeIndex.value !== null) {
-			const selectedDateTime = {
-				date: selectedDate.value,
-				time: timeSlot.value[selectedTimeIndex.value],
-			};
-			selectedTime.value = selectedDateTime.time;
+		if (selectedDate.value !== null && selectedTimeSlotIndex.value !== null) {
+			selectedTimeSlot.value = timeSlots.value[selectedTimeSlotIndex.value];
 			nextTab();
 		} else {
 			console.log("Please select a date and time slot before clicking Next.");
@@ -153,12 +153,14 @@
 	const scheduleEvent = async () => {
 		try {
 			isScheduling.value = true;
+
 			const booking = {
 				client_name: name.value,
 				client_email: email.value,
 				comments: details.value,
 				schedule_date: selectedDate.value,
-				time_slot: selectedTime.value,
+				time_slot_start: toRailwayTime(selectedTimeSlot.value.start),
+				time_slot_end: toRailwayTime(selectedTimeSlot.value.end),
 			};
 
 			(await bookingCol()).insertOne(booking);
@@ -171,19 +173,19 @@
 		}
 	};
 
-	const isSlotDisable = (index) => {
-		return disabledSlots.value.includes(timeSlot.value[index]);
-	};
-
 	const resetBooking = () => {
 		selectedDate.value = null;
-		selectedTimeIndex.value = null;
-		selectedTime.value = null;
+		selectedTimeSlotIndex.value = null;
+		selectedTimeSlot.value = null;
 		currentTab.value = 1;
 		name.value = null;
 		email.value = null;
 		details.value = null;
 		disabledSlots.value = [];
+	};
+
+	const isSlotDisable = (index) => {
+		return disabledSlots.value.includes(timeSlots.value[index].start);
 	};
 </script>
 
@@ -207,20 +209,22 @@
 								isSlotDisable(index) ? 'pointer-events-none' : ''
 							}`"
 							v-if="!isBookFetching"
-							v-for="(time, index) of timeSlot"
+							v-for="(slot, index) of timeSlots"
 							:key="index">
 							<div
 								:class="`w-full whitespace-nowrap  hover:text-white border  hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-bold rounded-lg px-5 py-2.5 text-center mb-2 cursor-pointer ${
 									isSlotDisable(index)
-										? 'bg-gray-200 text-gray-400 border-gray-400'
+										? 'bg-gray-200 text-gray-400 border-gray-400 '
 										: 'text-green-500 border-green-700'
 								}`"
-								@click="isSlotDisable ? selectTimeSlot(index) : () => {}">
-								{{ time }}
+								@click="
+									isSlotDisable(index) ? () => {} : selectTimeSlot(index)
+								">
+								{{ slot.start }}
 							</div>
 							<transition name="fade">
 								<div
-									v-if="selectedTimeIndex === index"
+									v-if="selectedTimeSlotIndex === index"
 									class="w-full text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-bold rounded-lg px-5 py-2.5 text-center mb-2 cursor-pointer"
 									@click="nextClicked">
 									Next
@@ -425,7 +429,8 @@
 							d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
 					</svg>
 					<p class="font-semibold">
-						<span>{{ selectedTime }}, </span>{{ formattedDate }}
+						<span>{{ selectedTimeSlot && selectTimeSlot.start }}, </span
+						>{{ formattedDate }}
 					</p>
 				</div>
 				<div class="flex items-center text-gray-600 mt-1">
